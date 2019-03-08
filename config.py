f86a2db916b7936b5da67c28a18c0fd4d0d533fe
Nacho Verdón <nverblaz@gmail.com>
2019-03-09 00:56:44 +0100

    config pls

---
+++ config.py
@@ -0,0 +1,12 @@
+CONFIG = {
+    # Path with the replays
+    'replays_folder': '',
+    # Path to Dolphin.exe (Slippi)
+    'dolphin': '',
+    # Path to Melee 1.02 .iso
+    'melee': '',
+    # Path to OBS folder
+    'obs_path': 'C:\\Program Files\\obs-studio\\bin\\64bit\\'
+    # Hotkey to stop OBS recording
+    'hotkey': ['ctrl', 'alt', 'end']
+}
